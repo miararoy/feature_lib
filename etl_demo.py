12c0df7a76c1d434b047f91ee61e08ceafb44005
miararoy <roy@empiricalhire.com>
2019-09-14 20:24:30 +0300

    revert

--- etl_demo.py
+++ etl_demo.py
@@ -1,7 +1,7 @@
 from feature_lib.etl_abc import AbstractEtl
 
 
-class ETL(AbstractEtl):
+class Etl(AbstractEtl):
     def extract(self):
         self.df.dropna(subset=["user_id"], inplace=True)
         self.df["creation_to_binding"] = self.df.binding_date - self.df.creation_date
