c956fdac003310251272d2bf66705b4c072213a8
miararoy <miararoy@gmail.com>
2019-09-15 00:53:11 +0300

    Update etl_demo.py

--- etl_demo.py
+++ etl_demo.py
@@ -5,5 +5,4 @@ class Etl(AbstractEtl):
     def extract(self):
         self.df.dropna(subset=["user_id"], inplace=True)
         self.df["creation_to_binding"] = self.df.binding_date - self.df.creation_date
-        self.df.set_index("user_id", inplace=True)
         return self.df
