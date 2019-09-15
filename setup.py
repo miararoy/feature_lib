2f13bef936a0f14aa7f72541e81a7f5984dcfc8f
miararoy <roy@empiricalhire.com>
2019-09-15 22:53:48 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='feature_lib',
+    url='https://github.com/miararoy/feature_lib',
+    author='Roy Miara',
+    author_email='miararoy@gmail.com',
+    packages=[
+        'etl_abc.AbstractEtl'
+    ],
+    install_requires=[
+        'pandas==0.25.0'
+    ],
+    version='1.0',
+    description='feature lib utils',
+    long_description=open('README.md').read(),
+)
